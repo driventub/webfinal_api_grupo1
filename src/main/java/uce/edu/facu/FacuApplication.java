9e007b948cd9146da658d9eda7988510e2843400
driven <aldazpaulo@gmail.com>
2023-08-22 11:19:57 -0500

    first commit

---
+++ src/main/java/uce/edu/facu/FacuApplication.java
@@ -0,0 +1,13 @@
+package uce.edu.facu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uce/edu/facu/FacuApplicationTests.java
@@ -0,0 +1,13 @@
+package uce.edu.facu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
